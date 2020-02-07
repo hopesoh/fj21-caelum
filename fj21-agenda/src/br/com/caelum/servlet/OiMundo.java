b20433a8fe7329e9962cda1cdce3f2cad7a3496a
Sonia Ribeiro <sonia.ribeiro@usp.br>
2020-02-06 21:46:07 -0300

    initial commit

--- fj21-agenda/src/br/com/caelum/servlet/OiMundo.java
+++ fj21-agenda/src/br/com/caelum/servlet/OiMundo.java
@@ -12,16 +12,16 @@ public class OiMundo extends HttpServlet{
 	
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		PrintWriter out = resp.getWriter();
 		
-		out.print("<html>");
-		out.print("<head>");
-		out.print("<title>Primeira Servlet </title>");
-		out.print("</head>");
-		out.print("<body>");
-		out.print("<h1>Oi mundo Servlet!!</h1>");
-		out.print("</body>");
-		out.print("</html>");
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Primeira servlet</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Oi mundo servlet</h1>");
+		out.println("</body>");
+		out.println("</html>");
 		
 	}
 
